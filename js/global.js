189c2c4a29d143eb1713d5e66418a9b330018924
lcs158-AI <37573094@qq.com>
2026-03-22 18:09:38 +0800

    限制级别14

--- js/global.js
+++ js/global.js
@@ -10,7 +10,9 @@ const viewer = new Cesium.Viewer('cesiumContainer', {
     baseLayerPicker: false,          // 隐藏底图选择器
     imageryProvider: false,          // 关键！禁用默认的 Cesium Ion 影像
     //terrainProvider: new Cesium.EllipsoidTerrainProvider(), // 平面地形（可后续升级）
-    terrain: Cesium.Terrain.fromWorldTerrain(),
+    terrain: Cesium.Terrain.fromWorldTerrain({
+        maximumLevel: 14,           // 限制最大级别为14（值越小加载越快，细节越少）
+    }),
     animation: false,                // 隐藏动画控件
     timeline: false,                 // 隐藏时间线
     infoBox: false,                  // 隐藏信息框
